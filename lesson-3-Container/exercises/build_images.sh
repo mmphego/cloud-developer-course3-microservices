b6e230af52e737882c945fc00ab8b30fffa55e30
Mpho Mphego <mpho112@gmail.com>
2020-03-02 07:55:28 +0200

    running audit fix first

--- lesson-3-Container/exercises/build_images.sh
+++ lesson-3-Container/exercises/build_images.sh
@@ -3,6 +3,7 @@
 find . -name 'Dockerfile' -print0 | while IFS= read -r -d '' line; do
     pushd $(dirname $line)
     rm -rf node_modules
+    npm audit fix
     npm i
     docker build --no-cache -t "$USER/$(basename $PWD)" .
     popd
