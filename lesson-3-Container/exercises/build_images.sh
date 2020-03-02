991455bbe1a371f43f3cc28f84b6d95dfc34c7a1
Mpho Mphego <mpho112@gmail.com>
2020-03-02 07:47:47 +0200

    delete before installing node_modules

--- lesson-3-Container/exercises/build_images.sh
+++ lesson-3-Container/exercises/build_images.sh
@@ -2,6 +2,7 @@
 
 find . -name 'Dockerfile' -print0 | while IFS= read -r -d '' line; do
     pushd $(dirname $line)
+    rm -rf node_modules
     npm i
     docker build --no-cache -t "$USER/$(basename $PWD)" .
     popd
