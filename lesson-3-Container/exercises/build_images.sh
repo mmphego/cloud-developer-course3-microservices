3c6948ff9d52bc596b147fe1afb269334ba1001a
Mpho Mphego <mpho112@gmail.com>
2020-03-02 06:57:30 +0200

    rebuild docker images everytime

--- lesson-3-Container/exercises/build_images.sh
+++ lesson-3-Container/exercises/build_images.sh
@@ -3,6 +3,6 @@
 find . -name 'Dockerfile' -print0 | while IFS= read -r -d '' line; do
     pushd $(dirname $line)
     npm i
-    docker build -t "$USER/$(basename $PWD)" .
+    docker build --no-cache -t "$USER/$(basename $PWD)" .
     popd
 done
